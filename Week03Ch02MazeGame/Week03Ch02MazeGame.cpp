1d169f9f574cfb64b0e6295634b391e00836c568
Dean Nevins <dean.nevins@activision.com>
2022-07-28 14:19:27 -0400

    Initial Commit

--- Week03Ch02MazeGame/Week03Ch02MazeGame.cpp
+++ Week03Ch02MazeGame/Week03Ch02MazeGame.cpp
@@ -1,48 +1,41 @@
 #include <iostream>
+#include "Game.h"
+#include "AudioManager.h"
 
 using namespace std;
 
-int GetIndexFromCoordinates(int x, int y, int width);
-void DrawLevel(char Level[], int width, int height);
-
 int main()
 {
-    constexpr int kWidth = 25;
-    constexpr int kHeight = 15;
+	Game myGame;
 
-    char levelArray[]{'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ','*','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ','|',
-                      '|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','D',' ',' ',' ',' ',' ',' ','X','|',
-                      '+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+'};
+	if (myGame.Load())
+	{
+		while (!myGame.IsGameOver())
+		{
+			myGame.Run();
+		}
 
-    DrawLevel(levelArray, kWidth, kHeight);
-}
+		if (myGame.DidUserQuit())
+		{
+			cout << "Thanks for playing!" << endl;
+		}
+		else if (myGame.GetPlayerLives() < 0)
+		{
+			cout << "YOU LOSE!!!!!!!!!" << endl;
+			AudioManager::GetInstance()->PlayLoseSound();
+		}
+		else
+		{
+			cout << "YOU WIN!!!!!!!!!!" << endl;
+			AudioManager::GetInstance()->PlayWinSound();
+		}
+	}
+	else
+	{
+		cout << "Game did not load. Terminating now!" << endl;
+	}
+	
+	AudioManager::DestroyInstance();
 
-int GetIndexFromCoordinates(int x, int y, int width)
-{
-    return x + y * width;
-}
-
-void DrawLevel(char level[], int width, int height)
-{
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            int index = GetIndexFromCoordinates(x, y, width);
-            cout << level[index] << " ";
-        }
-        cout << endl;
-    }
+	return 0;
 }
